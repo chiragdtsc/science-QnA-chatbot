20197e1ef5d08af9f9e2dd8544b0fd56f60c3206
Chirag <chirag.dtsc@gmail.com>
2022-12-21 23:45:56 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openfabric/openfabric-pyenv:0.1.9-3.8
+
+RUN mkdir cognitive-assistant
+WORKDIR /cognitive-assistant
+COPY . .
+RUN python3 -m pip install --upgrade pip
+RUN poetry install -vvv --no-dev
+RUN pip install -r requirements.txt
+EXPOSE 5000
+CMD ["sh","start.sh"]
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import warnings
+from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText 
+from openfabric_pysdk.context import OpenfabricExecutionRay
+from openfabric_pysdk.loader import ConfigClass
+from time import time
+from q_a import FindAnswer
+import logging
+
+############################################################
+# Callback function called on update config
+############################################################
+def config(configuration: ConfigClass):
+    # TODO Add code here
+    pass
+
+
+############################################################
+# Callback function called on each execution pass
+############################################################
+def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
+    output = []
+    for text in request.text:
+        # TODO Add code here
+        start = time()
+        qa_object = FindAnswer(text)
+        response = qa_object.extractKnowledge()
+        output.append(response)
+        end = time()
+        logging.info(f'Time taken: {end-start}')
+
+    return SimpleText(dict(text=output))
